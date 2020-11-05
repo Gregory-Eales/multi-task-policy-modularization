8592561af687c6a863c5bb91a589ba0eab281b61
Gregory Eales <gregoeales@aol.com>
2020-11-04 21:37:00 -0800

    Update test_loop.py

--- mtpm/utils/test_loop.py
+++ mtpm/utils/test_loop.py
@@ -30,6 +30,33 @@ def test_procgen(agent, env, n_steps, update_step):
 			agent.update()
 
 
+def test_multi_target(agent, env, n_steps, update_step):
+
+	rewards = []
+	firsts = []
+
+	_, prev_state, prev_first = env.observe()
+
+	for step in tqdm(range(n_steps)):
+
+		
+		action = agent.act(prev_state)
+		env.act(action)
+
+		reward, state, first = env.observe()
+
+		agent.store(prev_state, reward, prev_first)	
+
+		prev_state = state
+		prev_first = first
+
+		if step % update_step == 0 and step!=0:
+			agent.update()
+
+	return agent.get_rewards()
+
+
+
 def test(agent, env, n_steps, update_step):
 
 	rewards = []
