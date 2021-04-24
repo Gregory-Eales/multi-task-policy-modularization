97944f2e737fa2edd751462a6531f4369b773a7f
Gregory H Eales <gregoeales@aol.com>
2021-04-24 15:44:18 -0700

    define predefined experiments

---
+++ mtpm/utils/experiments.py
@@ -0,0 +1,40 @@
+'''
+this file contains functions for fixed predefined experiments
+'''
+
+def run_single_task_baseline():
+	
+	'''
+	runs agents on each vanilla task in gym3 vectorized env individually
+	'''
+
+	pass
+
+
+def run_single_env_mutitask_baseline():
+
+	'''
+	runs agents on one env the multi-task wrapper
+	'''
+
+	pass
+
+
+def run_simultaneous_multitask_baseline():
+
+	'''
+	runs agent on multiple environments simultaneously using the
+	multi-task wrapper
+	'''
+
+	pass
+
+
+def run_multitask_baseline():
+
+	'''
+	run agents on one env at a time randomly selecting a new env
+	and the end of each training session
+	'''
+
+	pass
